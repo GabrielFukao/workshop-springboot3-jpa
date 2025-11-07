b17f85b9605959287ae7b283ddd1f4f076aaabe9
Gabriel Fukao <gabrielfukao@hotmail.com>
2025-11-07 07:49:05 -0300

    correction

---
+++ src/main/java/gabrielfukao/curso/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package gabrielfukao.curso.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import gabrielfukao.curso.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
